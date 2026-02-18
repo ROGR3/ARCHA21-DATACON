4f16381698a5dad2e23592e64777cbfc0ee07cbf
Robin Grundel <robingrundel@seznam.cz>
2026-02-18 11:39:18 +0100

    3 years back cpzp created

--- matching_analysis.py
+++ matching_analysis.py
@@ -25,7 +25,7 @@ def main():
         zacatek_pojisteni=date(2015, 1, 1),
         konec_pojisteni=date(2023, 12, 31),
         year_offset=3,
-        use_local_cache=True,
+        use_local_cache=False,
     )
     data_loader = DataLoader(config)
     age_cohort_calculator = AgeCohortCalculator(config)
@@ -41,9 +41,9 @@ def main():
     groups = {
         PE_GROUP_NAMES.NEVER_PRESCRIBED: data_loader.never_prescribed_vax_people,
         PE_GROUP_NAMES.ZERO_PE_SUSPECTIBLE: data_loader.zero_pe_suspectible,
-        # PE_GROUP_NAMES.ZERO_PE: data_loader.zero_pe_vax_people,
-        # PE_GROUP_NAMES.ONE_TO_FIVE_HUNDRED_PE: data_loader.one_to_five_hundred_pe_vax_people,
-        # PE_GROUP_NAMES.FIVE_HUNDRED_TO_FIVE_THOUSAND_PE: data_loader.five_hundred_to_five_thousand_pe_vax_people,
+        PE_GROUP_NAMES.ZERO_PE: data_loader.zero_pe_vax_people,
+        PE_GROUP_NAMES.ONE_TO_FIVE_HUNDRED_PE: data_loader.one_to_five_hundred_pe_vax_people,
+        PE_GROUP_NAMES.FIVE_HUNDRED_TO_FIVE_THOUSAND_PE: data_loader.five_hundred_to_five_thousand_pe_vax_people,
     }
 
     for group_name, group in groups.items():
